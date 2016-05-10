f47f9ad5d0451a96da8a69f53de269fd83fcecab
clozanosanchez <clozanosanchez91@gmail.com>
2016-05-10 11:03:02 +0200

    Add is-dir dependency and remove CheckDirectory
    Changes to be committed:
    	modified:   lib/helpers/factories/RunnableServiceFactory.js
    	deleted:    lib/helpers/fs/CheckDirectory.js
    	modified:   lib/platform/ServiceManager.js
    	modified:   package.json
    	deleted:    test/cases/helpers/fs/CheckDirectoryTest.js
    	modified:   test/env/MicrorestModules.js

--- test/env/MicrorestModules.js
+++ test/env/MicrorestModules.js
@@ -10,28 +10,26 @@
  * @module
  */
 
-var CALLABLE_SERVICE_FACTORY_MODULE = process.cwd() + '/lib/helpers/factories/CallableServiceFactory';
-var RUNNABLE_SERVICE_FACTORY_MODULE = process.cwd() + '/lib/helpers/factories/RunnableServiceFactory';
-var CHECK_DIRECTORY_MODULE = process.cwd() + '/lib/helpers/fs/CheckDirectory';
-var CONFIGURATION_LOADER_MODULE = process.cwd() + '/lib/helpers/loaders/ConfigurationLoader';
-var SERVICE_CONTEXT_LOADER_MODULE = process.cwd() + '/lib/helpers/loaders/ServiceContextLoader';
-var SERVICE_FUNCTIONALITY_LOADER_MODULE = process.cwd() + '/lib/helpers/loaders/ServiceFunctionalityLoader';
-var LOGGER_MANAGER_MODULE = process.cwd() + '/lib/helpers/logging/LoggerManager';
-var CHECK_SCHEMA_MODULE = process.cwd() + '/lib/helpers/schemas/CheckSchema';
-var CREDENTIALS_GENERATOR_MODULE = process.cwd() + '/lib/helpers/security/CredentialsGenerator'
-var CLIENT_MODULE = process.cwd() + '/lib/platform/Client';
-var MICROREST_MODULE = process.cwd() + '/lib/platform/Microrest';
-var SERVER_MODULE = process.cwd() + '/lib/platform/Server';
-var SERVICE_MANAGER_MODULE = process.cwd() + '/lib/platform/ServiceManager';
-var CALLABLE_SERVICE_MODULE = process.cwd() + '/lib/services/CallableService';
-var RUNNABLE_SERVICE_MODULE = process.cwd() + '/lib/services/RunnableService';
-var SERVICE_MODULE = process.cwd() + '/lib/services/Service';
-var SERVICE_CONTEXT_MODULE = process.cwd() + '/lib/services/ServiceContext';
+const CALLABLE_SERVICE_FACTORY_MODULE = `${process.cwd()}/lib/helpers/factories/CallableServiceFactory`;
+const RUNNABLE_SERVICE_FACTORY_MODULE = `${process.cwd()}/lib/helpers/factories/RunnableServiceFactory`;
+const CONFIGURATION_LOADER_MODULE = `${process.cwd()}/lib/helpers/loaders/ConfigurationLoader`;
+const SERVICE_CONTEXT_LOADER_MODULE = `${process.cwd()}/lib/helpers/loaders/ServiceContextLoader`;
+const SERVICE_FUNCTIONALITY_LOADER_MODULE = `${process.cwd()}/lib/helpers/loaders/ServiceFunctionalityLoader`;
+const LOGGER_MANAGER_MODULE = `${process.cwd()}/lib/helpers/logging/LoggerManager`;
+const CHECK_SCHEMA_MODULE = `${process.cwd()}/lib/helpers/schemas/CheckSchema`;
+const CREDENTIALS_GENERATOR_MODULE = `${process.cwd()}/lib/helpers/security/CredentialsGenerator`;
+const CLIENT_MODULE = `${process.cwd()}/lib/platform/Client`;
+const MICROREST_MODULE = `${process.cwd()}/lib/platform/Microrest`;
+const SERVER_MODULE = `${process.cwd()}/lib/platform/Server`;
+const SERVICE_MANAGER_MODULE = `${process.cwd()}/lib/platform/ServiceManager`;
+const CALLABLE_SERVICE_MODULE = `${process.cwd()}/lib/services/CallableService`;
+const RUNNABLE_SERVICE_MODULE = `${process.cwd()}/lib/services/RunnableService`;
+const SERVICE_MODULE = `${process.cwd()}/lib/services/Service`;
+const SERVICE_CONTEXT_MODULE = `${process.cwd()}/lib/services/ServiceContext`;
 
-
-var CONFIGURATION_REAL_FILE = process.cwd() + '/configuration.json';
-var CONFIGURATION_TEST_FILE = process.cwd() + '/test/env/configuration.json';
-var CONFIGURATION_LOGGER_TEST_FILE = process.cwd() + '/test/env/configurationLogger.json';
+const CONFIGURATION_REAL_FILE = `${process.cwd()}/configuration.json`;
+const CONFIGURATION_TEST_FILE = `${process.cwd()}/test/env/configuration.json`;
+const CONFIGURATION_LOGGER_TEST_FILE = `${process.cwd()}/test/env/configurationLogger.json`;
 
 /**
  * Microrest Framework Modules for testing
@@ -44,7 +42,6 @@ var CONFIGURATION_LOGGER_TEST_FILE = process.cwd() + '/test/env/configurationLog
 module.exports = {
     callableServiceFactory: CALLABLE_SERVICE_FACTORY_MODULE,
     runnableServiceFactory: RUNNABLE_SERVICE_FACTORY_MODULE,
-    checkDirectory: CHECK_DIRECTORY_MODULE,
     configurationLoader: CONFIGURATION_LOADER_MODULE,
     serviceContextLoader: SERVICE_CONTEXT_LOADER_MODULE,
     serviceFunctionalityLoader: SERVICE_FUNCTIONALITY_LOADER_MODULE,
@@ -59,7 +56,6 @@ module.exports = {
     runnableService: RUNNABLE_SERVICE_MODULE,
     service: SERVICE_MODULE,
     serviceContext: SERVICE_CONTEXT_MODULE,
-
 
     configurationRealFile: CONFIGURATION_REAL_FILE,
     configurationTestFile: CONFIGURATION_TEST_FILE,
